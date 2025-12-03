5a2875a037b168a01b99fd96b48d94432e1a080b
Lucas Adriano <lukasadriano94@hotmail.com>
2025-12-03 11:21:50 -0300

    Inicializando repositorio para curso de orientacao a objetos e .dart

---
+++ test/meu_app_orientacao_a_objetos_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:meu_app_orientacao_a_objetos_dart/meu_app_orientacao_a_objetos_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
